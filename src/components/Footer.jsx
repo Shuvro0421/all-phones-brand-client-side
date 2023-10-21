e425800134ef7d5f544e8df086080af160c2daa8
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-10-21 15:58:02 +0600

    dark mode light mode has been coded

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <footer className="footer footer-center p-4 bg-red-500 text-white">
             <aside>
                 <p>Copyright © 2023 - Developed  by Md. Adib Arman Shuvro</p>
             </aside>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import Brand from "./Brand";
 
 const Home = () => {
     const [brands, setBrands] = useState([])
+
     useEffect(() => {
         fetch('http://localhost:5000/brands')
             .then(res => res.json())
@@ -25,6 +26,40 @@ const Home = () => {
                 <strong className="text-rose-500 mt-5">Aim Goal</strong>
                 <p>By the end of 2023, we are going to launch every single and most updated products in our website. Grossing up millions of dollars by selling the best product you can get and now, this will be more expanded</p>
             </div>
+            {/* offer */}
+            <h1 className="md:text-6xl text-4xl text-center my-3 text-rose-500 font-semibold">Deals and More!!</h1>
+            <div className="carousel w-full">
+
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img  src="https://img.global.news.samsung.com/in/wp-content/uploads/2018/03/PR_926x400_banner2.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle btn-ghost hover:text-rose-500">❮</a>
+                        <a href="#slide2" className="btn btn-circle btn-ghost hover:text-rose-500">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i0.wp.com/www.smartprix.com/bytes/wp-content/uploads/2021/04/mi-fan-fest-2021.png?fit=1200%2C675&ssl=1" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle btn-ghost hover:text-rose-500">❮</a>
+                        <a href="#slide3" className="btn btn-circle btn-ghost hover:text-rose-500">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://m-cdn.phonearena.com/images/article/139475-wide-two_1200/Best-Prime-Day-Google-Pixel-phone-deals-Superb-discounts-on-Pixel-phones-right-now.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle btn-ghost hover:text-rose-500">❮</a>
+                        <a href="#slide4" className="btn btn-circle btn-ghost hover:text-rose-500">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="https://9to5mac.com/wp-content/uploads/sites/6/2021/11/Untitled.jpg?quality=82&strip=all&w=1600" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle btn-ghost hover:text-rose-500">❮</a>
+                        <a href="#slide1" className="btn btn-circle btn-ghost hover:text-rose-500">❯</a>
+                    </div>
+                </div>
+            </div>
+            {/* contact us */}
             <div className="p-5 bg-blue-950 text-white md:m-5 m-2 mt-5 rounded-lg shadow-2xl">
                 <h1 className="md:text-6xl text-4xl text-center text-rose-500 font-semibold">Contact us</h1>
                 <div className="text-center">
